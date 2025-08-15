75dd0d274eae2cd3bc1ed7bef625747a4acce19b
Poh San <wangpohshan@gmail.com>
2025-08-15 15:14:59 +0800

    optimised goToCountry, now it waits for map to draw before zooming in
    also made the code less messy by abstracting stuff into fns

--- src/mapUtils/mapActions.ts
+++ src/mapUtils/mapActions.ts
@@ -1,3 +1,5 @@
+import { getCenter } from "ol/extent";
+import { toLonLat } from "ol/proj";
 import Map from "ol/Map";
 import Feature from "ol/Feature";
 
@@ -12,7 +14,6 @@ export function createGoToCountry(
       (f) => f.get("iso_3166_1_alpha_2_codes") === countryCode
     );
     if (matchFeature) {
-      console.log("Zooming in on country:", matchFeature);
       const coords = matchFeature.get("geo_point_2d");
       if (coords) {
         // const { lon, lat } = coords;
@@ -21,27 +22,42 @@ export function createGoToCountry(
 
         const view = map.getView();
         const extent = matchFeature.getGeometry()?.getExtent();
+        console.log("Extent is", extent);
         if (!extent) return;
 
-        // Step 1: Zoom out
-        view.animate({
-          zoom: 2,
-          duration: 1000,
-        });
-
-        // Step 2: Listen for features loaded
-        const onFeaturesLoaded = () => {
-          console.log("Features rendered, zooming in to target");
-          // Step 3: Zoom in to target
+        // create zoom fns
+        const zoomInToCountry = () => {
+          console.log("Zooming in to target");
           view.fit(extent, {
             duration: 2000,
             padding: [10, 10, 10, 10],
             maxZoom: 10,
           });
-          map.un("rendercomplete", onFeaturesLoaded); // Clean up listener
         };
 
-        map.on("rendercomplete", onFeaturesLoaded);
+        const zoomOut = () => {
+          console.log("Zooming out");
+          view.animate({
+            zoom: 2,
+            duration: 1000,
+          });
+        };
+
+        // Create event handler for detecting when features are loaded after zoom-out
+        const onFeaturesLoaded = () => {
+          console.log("Features rendered, waiting 2 secs");
+          setTimeout(() => zoomInToCountry(), 1400);
+        };
+
+        // initiate zoom sequence
+        const currZoomLevel = view.getZoom();
+        console.log("Current zoom level:", currZoomLevel);
+        if (currZoomLevel && currZoomLevel > 2) {
+          map.once("rendercomplete", onFeaturesLoaded);
+          zoomOut();
+        } else {
+          zoomInToCountry();
+        }
       }
     }
   };
